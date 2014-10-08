c42fa0ed9cc43c9d8b93a5034622d5aad56a463d
Moiz K. Malik <moiz@heuretix.com>
2014-10-07 23:44:05 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -2,15 +2,12 @@ $(function() {
     var countdown = new Countdown({
         selector: '#timer',
         msgBefore: "The Final Countdown!",
-        msgAfter: "150 Years!",
+        msgAfter: "Celebrating 150 Years of vibrancy",
         msgPattern: "{days} days, {hours} hours, {minutes} minutes and {seconds} seconds!",
         dateStart: new Date('2014/09/26 12:00'),
         dateEnd: new Date('Oct 15, 2015 12:00'),
         onStart: function() {
-          console.log('Merry Christmas!');
-        },
-        onEnd: function() {
-          console.log('Happy New Year!');
+          console.log('Celebrating 150 Years!');
         }
     });
 });
